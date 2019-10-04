f1eda02b55e345219112b71940a73577772e421d
Atlchris67 <43253076+Atlchris67@users.noreply.github.com>
2019-10-04 17:10:18 -0400

    update-cost-checkin

--- js/script.js
+++ js/script.js
@@ -2,46 +2,149 @@
 //the first text field should be in focus by default.
 $("#name").focus();
 $('#other-title').hide();
+$('.activities').append('<div id="total"></div>');
 
 // ”Job Role” section
 // On job role change if other show input feild
-$( "#title" ).change(function(event) {
-    const titleSelect  = this.value;
+$("#title").change(function (event) {
+    const titleSelect = this.value;
     const otherInput = $("#other-title");
-    (titleSelect === 'other')? $('#other-title').show():$('#other-title').hide().val('')  ;
-  });
+    (titleSelect === 'other') ? $('#other-title').show(): $('#other-title').hide().val('');
+});
 
 
 // ”T-Shirt Info” section
 // Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field reads “Please select a T-shirt theme”.
-$( "#design" ).change(function(event) {
-    const themeSelect  = this.value;
+$("#design").change(function (event) {
+    const themeSelect = this.value;
     const colorInput = $("#color");
 
     colorInput.empty();
-    switch (themeSelect){
+    switch (themeSelect) {
         case "js puns":
-                colorInput.append(`<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+            colorInput.append(`<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
                 <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
                 <option value="gold">Gold (JS Puns shirt only)</option>`);
             break;
-        case  "heart js":
-                colorInput.append(`<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+        case "heart js":
+            colorInput.append(`<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
                 <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
-                <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>` );
+                <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`);
             break;
         default:
-                colorInput.append(`<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+            colorInput.append(`<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
                 <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
                 <option value="gold">Gold (JS Puns shirt only)</option> 
                 <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
                 <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
                 <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`);
     }
+
+});
+
+
+function setStatus(workshop, stateToSet){
+    if (stateToSet =="disabled"){
+        workshop.prop("checked", false);
+        workshop.prop("disabled", true);
+        workshop.parent().addClass("disabled");
+        workshop.parent().append('<span class="disabled" style="color:red;">Unavailable</span>');
+    } else {
+        workshop.prop("disabled", false);
+        workshop.parent().removeClass("disabled");
+        workshop.parent().children('span').remove();
+    }
+};
+
+$("form input:checkbox").change(function (event) {
+    const $events = $(":checkbox");
+    const $checkedEvent = event.target;
+    const eventName = this.name;
+    const eventDateTime = $(this).data("day-and-time");
+    console.log(eventDateTime);
+
+
+    // if all checked, disable the rest
+    if ((eventName === "all") && $(this).prop('checked')) {
+        $events.each(function () {
+            if (this.name !== 'all') {
+                setStatus($(this), "disabled");                
+            };
+        });
+    };
+
+    //if all unchecked enable all others
+    if ((eventName === "all") && !$(this).prop('checked')) {
+        $events.each(function () {
+            if (this.name !== 'all') {
+                setStatus($(this), "enabled");     
+            };
+        });
+    };
+
+    //if something besides all checked, 
+    if ((eventName !== "all") && $(this).prop('checked')) {
+            $events.each(function () {
+            const thisDateTime = $(this).data("day-and-time");
+            //disable all, if not disabled already
+            if ((this.name === 'all') && ( !$(this).parent().hasClass("disabled"))) {
+                setStatus($(this), "disabled");
+            };
+
+            //check others with same datetime data attribute and disable if required
+            if (this.name !== 'all' && (this.name !== eventName)  && (thisDateTime === eventDateTime) && ( !$(this).parent().hasClass("disabled"))) {
+                setStatus($(this), "disabled");;
+            };
+        });
+    };
+
+
+    //if an item is unchecked, potentially enable new NON conflicts 
+    if ((eventName !== "all") && !$(this).prop('checked')) {
+        $events.each(function () {
+            const thisDateTime = $(this).data("day-and-time");
+            if ((this.name !== 'all') && (thisDateTime === eventDateTime)) {
+                setStatus($(this), "enabled");
+            };
+        });
+    };
+
+    //if an item is unchecked, potentially enable all and other conflicts 
+    if ((eventName !== "all") && !$(this).prop('checked')) {
+
+        let allStatus = true;
+        let allWorkshops;
+        //check all the other items to see if they are checked
+        $events.each(function () {
+            if ((this.name !== 'all') && ($(this).prop('checked'))) {
+                allStatus = false;
+            };
+            if (this.name === 'all'){
+                allWorkshops = this;
+            }
+        });
+        //if all clear, enable all workshop checkbox
+        if (allStatus){
+            setStatus($(allWorkshops), "enabled");
+        }
+
+    };
+
+    //Calculate current cost
+    let cost = 0;
+    $events.each(function () { 
+        if ($(this).prop('checked')){
+            cost +=   Number.parseInt(this.getAttribute("data-cost").replace(/\$/g, ''), 10);            
+        };       
+    });
     
-  });
+    //Display the total cost for activities
+    $("#total").html('<div id="total">Total: $' + cost + '.00</div>');
+    
+     
 
-
+      
+});
 // ”Register for Activities” section
 // Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
 // When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
@@ -82,4 +185,3 @@ $( "#design" ).change(function(event) {
 // Avoid use alerts for your validation messages.
 
 // If a user tries to submit an empty form, there should be an error indication or message displayed for the name field, the email field, the activity section, and the credit card fields if credit card is the selected payment method.
-
